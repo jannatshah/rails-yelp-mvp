d43e671a28615fc50352b4714fe52ecd24cf7534
Jannat Shah <jannat.shah@gmail.com>
2019-11-13 14:19:09 +0000

    Finished the views together with the refactoring so there is one less route for reviews#new

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,6 +6,7 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
     @reviews = @restaurant.reviews
+    @review = Review.new
   end
 
   def new
@@ -17,7 +18,6 @@ class RestaurantsController < ApplicationController
     if @restaurant.save
       redirect_to @restaurant
     else
-      # redirect_to new_restaurant_path
       render :new
     end
   end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,26 @@
 class ReviewsController < ApplicationController
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-    @review.restaurant = @restaurant
-  end
+  before_action :set_restaurant
+
+  # def new
+  #   @review = Review.new
+  #   @review.restaurant = @restaurant
+  # end
 
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
       redirect_to @restaurant
     else
-      # redirect_to new_restaurant_review_path
       render :new
     end
   end
 
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 
   def review_params
     params.require(:review).permit(:content, :rating)
