8acca656b1ed38397150e906ce4b7c01f3ae0b21
Jannat Shah <jannat.shah@gmail.com>
2019-11-13 13:48:57 +0000

    Rake is clean, now making the views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Categories: [chinese italian japanese french belgian]
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian'
+  },
+  {
+    name:         'Quilon',
+    address:      'Buckingham Gate, St James Park, Victoria, London SW6',
+    category:     'chinese'
+  },
+  {
+    name:         'Padella',
+    address:      'London Bridge, London SE1',
+    category:     'italian'
+  },
+  {
+    name:         'Wahaca',
+    address:      'One New Change, St Pauls, London EC2',
+    category:     'italian'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
